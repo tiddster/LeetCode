dff9b3938e02253ec3adcbecb0f0c722546768d1
tiddster <864187149@qq.com>
2021-09-09 21:21:32 +0800

    21.9.9

---
+++ T1460.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class T1460 {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        return Arrays.equals(target,arr);
+    }
+}
