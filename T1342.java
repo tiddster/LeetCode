1f397dd231dcff073478f0703dc75d9d4ee633e3
tiddster <864187149@qq.com>
2021-09-16 09:33:14 +0800

    21.9.16

---
+++ T1342.java
@@ -0,0 +1,17 @@
+/*
+给你一个非负整数 num ，请你返回将它变成 0 所需要的步数。 如果当前数字是偶数，你需要把它除以 2 ；否则，减去 1 。
+ */
+public class T1342 {
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while (num!=0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                --num;
+            }
+            ++count;
+        }
+        return count;
+    }
+}
