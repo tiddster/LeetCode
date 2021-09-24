2a2dd69f3f03de4d8cc1276a96277ed85f952e26
tiddster <864187149@qq.com>
2021-09-24 23:55:20 +0800

    21.9.24

---
+++ T242.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class T242 {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars, tChars);
+    }
+}
