04bd0002195bc67b9ebaf377cc045575df5116b5
tiddster <864187149@qq.com>
2021-11-30 16:51:22 +0800

    21.11.30 hashmap

--- T01_2.java
+++ T01_2.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class T01_2 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class T01_2 {
+    public int[] twoSum(int[] nums, int target) {
+        if(nums.length == 0){
+            return new int[]{};
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int temp = target - nums[i];
+            if(map.containsKey(temp)){
+                return new int[]{map.get(temp),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
 }
