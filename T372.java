e5f38a795268337962bc5ded54d38f945ee60d32
tiddster <864187149@qq.com>
2021-12-05 20:39:04 +0800

    21.12.5 快速幂 取模公式

--- T372.java
+++ T372.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class T372 {
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+public class T372 {
+    int base = 1337;
+    public int superPow(int a, int[] b) {
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        for(int num : b)
+            queue.addLast(num);
+        return superPow(a, queue);
+    }
+
+    private int superPow(int a, Deque<Integer> queue){
+        if(queue.isEmpty())
+            return 1;
+
+        int lastBit = queue.removeLast();
+
+        int part1 = quickPow(a, lastBit);
+        int part2 = quickPow(superPow(a,queue), 10);
+
+        return (part1 * part2) % base;
+    }
+
+    private int quickPow(int a, int b){
+        if(b == 0)
+            return 1;
+
+
+        if(b % 2 == 0)
+            return quickPow( ((a % base) * (a % base)) % base, b / 2);
+        else
+            return ( (a % base) * quickPow(a,b-1) ) % base;
+    }
 }
