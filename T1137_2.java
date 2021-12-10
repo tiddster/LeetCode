d69d4ca5ff3c44127e456a1747372f5182326376
tiddster <864187149@qq.com>
2021-12-10 14:33:50 +0800

    21.12.10 dp

---
+++ T1137_2.java
@@ -0,0 +1,12 @@
+public class T1137_2 {
+    public int tribonacci(int n) {
+        int dp[] = new int[n+3];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3; n>=3&&i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
+    }
+}
