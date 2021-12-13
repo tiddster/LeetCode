34654ea3e68a616ad0342c24632bde2cd67c4d17
tiddster <864187149@qq.com>
2021-12-13 12:55:31 +0800

    21.12.13

---
+++ O95.java
@@ -0,0 +1,27 @@
+import static java.lang.Math.max;
+
+/*
+给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+
+一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+
+例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ */
+public class O95 {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1][len2];
+        for(int i=0; i<len1; i++){
+            for(int j=0; j<len2; j++){
+                if(text1.toCharArray()[i] == text2.toCharArray()[j]){
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                }else{
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
