5c2b99227d0ba277f27061b9d0329f0fd7aba793
tiddster <864187149@qq.com>
2022-04-02 12:43:55 +0800

    22.4.2

---
+++ T96.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0] * (n+1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2,n+1):
+            for j in range(1,n+1):
+                dp[i] += dp[i-j]*dp[j-1]
+        return dp[n]
