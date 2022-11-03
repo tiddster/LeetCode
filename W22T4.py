f288fd4f6eb6f4e1d23e60e4005d5b4da16f149d
tiddster <864187149@qq.com>
2022-11-03 20:27:18 +0800

    22.11.3

--- W22T4.py
+++ W22T4.py
@@ -3,9 +3,9 @@ def main():
     B = input()
     ans = 0
     for i in range(len(A)):
-        if A[i] == 1 and B[i] == 0:
+        if A[i] == '1' and B[i] == '0':
             ans += 1
-        elif A[i] == 0 and B[i] == 1:
+        elif A[i] == '0' and B[i] == '1':
             print(0)
             pass
     print(2 ** ans)
